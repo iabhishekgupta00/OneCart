788693e75c739631d55dae7b3cfb5bd194f0b790
Abhishek Gupta <shabhishekgupta890@gmail.com>
2025-07-05 23:18:56 +0530

    Update AuthContext.jsx

--- onecart/admin/src/context/AuthContext.jsx
+++ onecart/admin/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext } from "react"
 export const authDataContext = createContext()
 
 function AuthContext({ children }) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://oneca-backend.onrender.com"
     let value = {
         serverUrl
     }
